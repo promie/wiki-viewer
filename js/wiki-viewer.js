9113dacc6ca1d0a30ee38c52588e6189895ec85a
Promie Yutasane <promie.yutasane@gmail.com>
2017-11-11 13:06:33 +1100

    API Link Complete

--- js/wiki-viewer.js
+++ js/wiki-viewer.js
@@ -2,7 +2,29 @@
 
 $(document).ready(function(){
 
+    //const searchBox = $('#search-box').val();
+
+    const testBox = 'Bruno Mars';
     
+    const url = `https://en.wikipedia.org/w/api.php?action=opensearch&search=${testBox}&format=json&callback=?`
+    
+    $.ajax({
+        
+        type: "GET",
+        url: url,
+        contentType: "application/json; charset=utf-8",
+        async: false,
+        dataType: "json",
+        success: function(data){
 
+            console.log(data);
+            console.log(data[0]);
+            console.log(data[1][0]);
+            console.log(data[2][0]);
+            console.log(data[3][0]);
+    
+        }
 
-});
+    });
+
+}); 
