5f28a01b461998984443e66ee7190a6bb3afd71e
Promie Yutasane <promie.yutasane@gmail.com>
2017-11-11 16:26:29 +1100

    Added github Logo

--- js/wiki-viewer.js
+++ js/wiki-viewer.js
@@ -13,7 +13,7 @@ $(document).ready(function(){
                 dataType: "json",
                 success: function(data){
                     if(searchBox!==''){
-                        $('#result').show();                    
+                        $('#result').show();    
                         displayData(data);        
                     }
                 }
@@ -38,8 +38,6 @@ $(document).ready(function(){
            $.each(data[1], function(val){
                 $('#result').append(`<li><a href="${data[3][val]}">${data[1][val]}</a><p>${data[2][val]}</p></li>`);
            });
-
-           console.log($('#result').length);
     } 
           
     }); 
