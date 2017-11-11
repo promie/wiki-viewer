e9638ea027c406aa359764c3255855f05548d149
Promie Yutasane <promie.yutasane@gmail.com>
2017-11-11 15:45:12 +1100

    Minor bugs fixed

--- js/wiki-viewer.js
+++ js/wiki-viewer.js
@@ -1,30 +1,45 @@
-//let url = "https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&srsearch=Bruno%20Mars"
-
 $(document).ready(function(){
 
-    //const searchBox = $('#search-box').val();
+    $('#search-button').on('click', function(){
 
-    const testBox = 'Bruno Mars';
-    
-    const url = `https://en.wikipedia.org/w/api.php?action=opensearch&search=${testBox}&format=json&callback=?`
-    
-    $.ajax({
-        
-        type: "GET",
-        url: url,
-        contentType: "application/json; charset=utf-8",
-        async: false,
-        dataType: "json",
-        success: function(data){
+            const searchBox = $('#search-box').val();        
+            const url = `https://en.wikipedia.org/w/api.php?action=opensearch&search=${searchBox}&format=json&callback=?`
+            
+            $.ajax({
+                type: "GET",
+                url: url,
+                contentType: "application/json; charset=utf-8",
+                async: false,
+                dataType: "json",
+                success: function(data){
+                    if(searchBox!==''){
+                        $('#result').show();                    
+                        displayData(data);        
+                    }
+                }
+            });
+    })
 
-            console.log(data);
-            console.log(data[0]);
-            console.log(data[1][0]);
-            console.log(data[2][0]);
-            console.log(data[3][0]);
-    
-        }
-
+    $('#magnify').on('click', function(){
+        $(this).hide();
+        $('#search-box').show(1000);
+        $('#search-box').val('');
     });
 
-}); 
+    $('#clear-button').on('click', function(){
+        $('#result').hide(100);
+        $('#search-box').hide(100);
+        $('#magnify').show(1000)
+        $('#search-box').val('');
+    });
+
+    const displayData = (data) =>{
+        
+           $.each(data[1], function(val){
+                $('#result').append(`<li><a href="${data[3][val]}">${data[1][val]}</a><p>${data[2][val]}</p></li>`);
+           });
+
+           console.log($('#result').length);
+    } 
+          
+    }); 
